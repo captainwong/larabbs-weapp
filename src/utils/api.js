ca722d4878219f7c814bbc9a6716b28a705b093c
vagrant-homestead <qtk.jack@gmail.com>
2018-12-03 14:47:46 +0000

    page user

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,69 +1,77 @@
 import wepy from 'wepy'
 
+const showServerError = (err) => {
+    console.log(err)
+    wepy.showModal({
+        title: '提示',
+        content: '服务器错误，请联系管理员或重试\r\n' + err
+    })
+}
+
 // 服务器接口地址
 const host = 'http://larabbs.test/api'
 
 // 普通请求
 const request = async(options, showLoading = true) => {
-  // 简化开发，如果传入字符串则转换成对象
-  if(typeof options === 'string'){
-    options = {
-        url: options
+    // 简化开发，如果传入字符串则转换成对象
+    if(typeof options === 'string'){
+        options = {
+            url: options
+        }
     }
-  }
 
-  // 显示加载中
-  if(showLoading){
-    wepy.showLoading({title: '加载中'})
-  }
+    // 显示加载中
+    if(showLoading){
+        wepy.showLoading({title: '加载中'})
+    }
 
-  // 拼接url
-  options.url = host + '/' + options.url;
-  console.log(options)
-  
-  // 调用小程序的 request 方法
-  let response = await wepy.request(options)
-  console.log(response)
+    // 拼接url
+    options.url = host + '/' + options.url;
+    console.log(options)
 
-  if(showLoading){
-    // 隐藏‘加载中’
-    wepy.hideLoading()
-  }
+    // 调用小程序的 request 方法
+    let response = await wepy.request(options)
+    console.log(response)
 
-  // 服务器异常后给予提示
-  if(response.statusCode === 500){
-    wepy.showModal({
-      title: '提示',
-      content: '服务器错误，请联系管理员或重试: 500 ' + response.data.message
-    })
-  }
+    if(showLoading){
+        // 隐藏‘加载中’
+        wepy.hideLoading()
+    }
 
-  return response
+    // 服务器异常后给予提示
+    if(response.statusCode === 500){
+        wepy.showModal({
+            title: '提示',
+            content: '服务器错误，请联系管理员或重试: 500 ' + response.data.message
+        })
+    }
+
+    return response
 }
 
 // 登录
 const login = async (params = {}) => {
-  // code 只能使用一次，所以每次单独调用
-  let loginData = await wepy.login()
-  console.log(loginData)
+    // code 只能使用一次，所以每次单独调用
+    let loginData = await wepy.login()
+    console.log(loginData)
 
-  // 参数中增加 code
-  params.code = loginData.code
+    // 参数中增加 code
+    params.code = loginData.code
 
-  // 接口请求 weapp/authorizations
-  let authResponse = await request({
-    url: 'weapp/authorizations',
-    data: params,
-    method: 'POST'
-  })
+    // 接口请求 weapp/authorizations
+    let authResponse = await request({
+        url: 'weapp/authorizations',
+        data: params,
+        method: 'POST'
+    })
 
-  // 登录成功，记录 token 信息
-  if(authResponse.statusCode === 201){
-    wepy.setStorageSync('access_token', authResponse.data.access_token)
-    wepy.setStorageSync('access_token_expired_at', new Date().getTime() + authResponse.data.expires_in * 1000)
-  }
+    // 登录成功，记录 token 信息
+    if(authResponse.statusCode === 201){
+        wepy.setStorageSync('access_token', authResponse.data.access_token)
+        wepy.setStorageSync('access_token_expired_at', new Date().getTime() + authResponse.data.expires_in * 1000)
+    }
 
-  return authResponse
+    return authResponse
 }
 
 // 刷新token
@@ -140,9 +148,10 @@ const logout = async (params = []) => {
 }
 
 export default {
-  request,
-  authRequest,
-  refreshToken,
-  login,
-  logout
+    showServerError,
+    request,
+    authRequest,
+    refreshToken,
+    login,
+    logout
 }
