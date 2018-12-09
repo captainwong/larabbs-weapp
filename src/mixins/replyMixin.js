eff4bb56ca5ee8d676ddfda6244e879c6a600c34
vagrant-homestead <qtk.jack@gmail.com>
2018-12-09 09:14:40 +0000

    reply delete

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -26,8 +26,10 @@ export default class ReplyMixin extends wepy.mixin {
 
             if(repliesResponse.statusCode === 200){
                 let replies = repliesResponse.data.data
-                replies.forEach(function(reply){
+                let user = await this.$parent.getCurrentUser()
+                replies.forEach((reply) => {
                     reply.created_at_diff = util.diffForHumans(reply.created_at)
+                    reply.can_delete = this.canDelete(user, reply)
                 })
                 this.replies = reset ? replies : this.replies.concat(replies)
                 let pagination = repliesResponse.data.meta.pagination
@@ -58,5 +60,45 @@ export default class ReplyMixin extends wepy.mixin {
         await this.getReplies()
         this.isLoading = false
         this.$apply()
+    }
+
+    canDelete(user, reply) {
+        if(!user)return false;
+        return (reply.user_id === user.id);
+    }
+
+    methods = {
+        async deleteReply(topicId, replyId){
+            let res = await wepy.showModal({
+                'title': '确认删除',
+                'content': '您确认删除该回复吗？',
+                comfirmText: '删除',
+                cancelText: '取消'
+            })
+
+            if(!res.confirm){return;}
+
+            try{
+                let deleteResponse = await api.authRequest({
+                    url: 'topics/' + topicId + '/replies/' + replyId,
+                    method: 'DELETE'
+                })
+
+                if(deleteResponse.statusCode === 204){
+                    wepy.showToast({
+                        title: '删除成功',
+                        icon: 'success',
+                        duration: 2000
+                    })
+
+                    this.replies = this.replies.filter((reply) => reply.id !== replyId);
+                    this.$apply()
+                }
+
+                return deleteResponse
+            }catch(err){
+                api.showServerError(err)
+            }
+        }
     }
 }
